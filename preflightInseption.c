4b3b897af7e0774b4e078c0f60ed2ca905a88f17
Osman Özcan <73670053+OsmanOzcan@users.noreply.github.com>
2022-03-11 22:12:18 +0300

    Add files via upload

--- preflightInseption.c
+++ preflightInseption.c
@@ -1,12 +1,20 @@
-//Compile => sudo gcc preflightInseption.c -o preflightInseption
-
 #include <stdlib.h>
 
 int main(){
 	
-	system("sudo killall flightSoftware servod buZZers");
+	system("sudo killall servod flightSoftware buZZers bno055 gps ina219 ms5611 raspivid verticalStabilization yawStabilization descentVelocity");
 	system("sudo truncate -s 0 telemetry.txt");
 	system("sudo truncate -s 0 OOPROM.txt");
+	system("sudo truncate -s 0 GCS.txt");
+	system("sudo truncate -s 0 GPSdatas.txt");
+	system("sudo truncate -s 0 BNO055datas.txt");
+	system("sudo truncate -s 0 INA219datas.txt");
+	system("sudo truncate -s 0 MS5611datas.txt");
+	system("sudo truncate -s 0 statuDatas.txt");
+	system("sudo truncate -s 0 motorPwm.txt");
+	system("sudo truncate -s 0 ina219/INA219datas.txt");
+	system("sudo rm /home/pi/video.mp4");
+	system("sudo rm /home/pi/canligoruntu.h264");
 	
 	
 	return 0;
